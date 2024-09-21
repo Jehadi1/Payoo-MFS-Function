7a506642d89e5d0638485eb1f2169e06b5e69fe3
Jehadi1 <mdjehadihasan75@gmail.com>
2024-09-21 20:32:53 +0600

    added this code

--- js/addMoney.js
+++ js/addMoney.js
@@ -14,15 +14,31 @@ document.getElementById('btn-add')
             const addMoney = inputById('add-amount');
             const addPin = inputById('add-pin');
             // console.log(addMoney, addPin);
-
+            if(isNaN(addMoney)){
+                alert('Failed to add Money');
+                return;
+            }
 
             if(addPin === 1234){
                 const balance =textById('account-balance');
+
+
                 const updateBalance = balance + addMoney;
-                console.log(updateBalance);
+                // console.log(updateBalance);
 
                 document.getElementById('account-balance')
                 .innerText = updateBalance;
+
+
+                const p = document.createElement('p');
+                p.innerText = `Added: ${addMoney}Tk. New Balance: ${updateBalance}Tk.`
+                console.log(p);
+
+                document.getElementById('transetion-container').appendChild(p);
+
+
+
+
             }
             else{
                 alert('Worng Pin')
--- js/cashOut.js
+++ js/cashOut.js
@@ -5,13 +5,36 @@ document.getElementById('btn-out')
      
             const outMoney = inputById('out-amount');
             const outPin = inputById('out-pin');
+
+            if(isNaN(outMoney)){
+                alert('Failed to add Money');
+                return;
+            }
             
             if(outPin === 1234){
                 const balance =textById('account-balance');
+
+                if(outMoney > balance){
+                    alert('Your account money is low');
+                    return;
+                }
+
                 const updateBalance = balance - outMoney;
                 
                 document.getElementById('account-balance')
                 .innerText = updateBalance;
+
+
+                const div = document.createElement('div');
+                div.classList.add('bg-blue-300');
+                div.innerHTML =  `
+                <h3 class="text-2xl font-bold text-center">Cash Out</h3>
+                <p>Cash Out: ${outMoney}Tk. New Balance: ${updateBalance}Tk.</p>`
+               
+
+                document.getElementById('transetion-container').appendChild(div);
+
+
             }
             else{
                 alert('Worng Pin')
